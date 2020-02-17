ffc28e06417c32a2bd11ce57903d35432b3cd054
Asit Kachhap <asit.kachhap@nagarro.com>
2020-02-17 18:47:03 +0530

    Changed items

--- Startup.cs
+++ Startup.cs
@@ -27,7 +27,7 @@ namespace azureappservicedemo
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Azure World!");
             });
         }
     }
